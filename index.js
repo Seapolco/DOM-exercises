33caaaa8fa313259b0f7dd18f4ce74a39a8685a3
Seapolco <seapolco@gmail.com>
2022-09-04 12:31:35 +0100

    Added a rudimentary time tracker function.
    To be utilised in a new project, will make a simple version of clockify.

--- index.js
+++ index.js
@@ -27,5 +27,52 @@ pinkDiv.appendChild(divP);
 
 blueH3.insertAdjacentElement('afterend', pinkDiv);
 
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => {
+    alert('Hello World');
+    e.target.style.background = 'yellow';
+});
+
+const tracker = document.querySelector(".tracker");
+tracker.style.backgroundColor = 'Green';
+tracker.textContent = 'Start';
+
+let timeSpent = document.querySelector(".timeSpent");
+
+let times = [];
+
+let start, stop, elapsed;
+
+tracker.addEventListener('click', (e) => {
+   // let times = [];
+   
+
+
+    if(e.target.textContent === 'Start') {
+        e.target.textContent = 'Stop';
+        e.target.style.backgroundColor = 'red';
+        start = new Date().getTime();
+        console.dir(start)
+        //times.push(start)
+    } else {
+        e.target.textContent = 'Start';
+        e.target.style.backgroundColor = 'Green';
+        //stop = new Date();
+        //console.log(stop)
+        elapsed = new Date().getTime() - start;
+        let hundredElapsed = elapsed
+        let roundedElapsed = Math.round((hundredElapsed /1000));
+        let date = new Date(null);
+        date.setSeconds(roundedElapsed);
+
+        let format = date.toISOString().substring(11,19)
+        //.substr(11, 8);
+        timeSpent.textContent = format
+        console.log(typeof format);
+    }
+
+});
+
 
 
